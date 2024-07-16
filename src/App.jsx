fc3b59c881d5d7c50d2444447e43a99c14ecc48a
“JorgePalacioD” <andyfac@hotmail.com>
2024-07-16 11:28:48 -0500

    Creacion de gif trending y buscador

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import Card from './components/Card/Card';
 
-const URL = 'https://rickandmortyapi.com/api/character/';
+const GIPHY_API_KEY = 'LeuS1PAyEtRvOAinsXSawT7uDjePHAym';
+const SEARCH_ENDPOINT = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&limit=20`;
+const TRENDING_ENDPOINT = `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}&limit=20`;
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [query, setQuery] = useState('');
+  const [gifs, setGifs] = useState([]);
 
   useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setCharacters(data.results);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchCharacters();
+    handleFetchTrendingGifs();
   }, []);
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+  const handleFetchTrendingGifs = async () => {
+    try {
+      const response = await fetch(TRENDING_ENDPOINT);
+      const data = await response.json();
+      setGifs(data.data);
+    } catch (error) {
+      console.error('Error fetching trending GIFs:', error);
+    }
   };
 
-  const filteredCharacters = characters.filter(character =>
-    character.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleFetchGifs = async () => {
+    try {
+      const response = await fetch(`${SEARCH_ENDPOINT}&q=${query}`);
+      const data = await response.json();
+      setGifs(data.data);
+    } catch (error) {
+      console.error('Error fetching GIFs:', error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    handleFetchGifs();
+  };
 
   return (
-    <div className='app'>
-      <div className="mb-4">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Search by name"
-          className="border p-2 w-full"
-        />
-      </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-        {filteredCharacters.map(character => (
-          <Card
-            key={character.id}
-            nameCharacter={character.name}
-            imgCharacter={character.image}
-            genderCharacter={character.gender}
-          />
-        ))}
+    <div className="min-h-screen bg-slate-300 p-8">
+      <div className="h-full mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+        <div className="p-8">
+          <form onSubmit={handleSearch} className="flex items-center space-x-4">
+            <input
+              type="text"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="Search for GIFs"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            >
+              Search
+            </button>
+          </form>
+          <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {gifs.map((gif) => (
+              <div key={gif.id} className="flex justify-center">
+                <img
+                  src={gif.images.fixed_height.url}
+                  alt={gif.title}
+                  className="rounded-md"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
